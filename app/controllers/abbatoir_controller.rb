02bd825f04753b79d73c7f52333877184bb05a29
Rhian <rhianatwork@gmail.com>
2013-01-07 17:12:23 +0100

    removed files

--- app/controllers/abbatoir_controller.rb
+++
@@ -1,5 +0,0 @@
-class AbbatoirController < ApplicationController
-  def map
-    @json = Abbatoir.all.to_gmaps4rails
-  end
-end
--- app/models/Abbatoir.rb
+++
@@ -1,8 +0,0 @@
-class Abbatoir
-  include DataMapper::Resource
-  include DataMapper::GeoKit
-  property :id, Serial, :key => true
-  property :name, String
-  property :address, String
-  property :postcode, String
-end
